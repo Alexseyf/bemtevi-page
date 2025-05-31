29889b1bb96e47b4773001e76d46a52add095aa5
Alexandre Seyffert <alexseyf66@gmail.com>
2025-05-31 18:21:58 -0300

    Corrigindo problemas de estilo e carregamento de imagens

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    unoptimized: true, // Isso permite que as imagens sejam renderizadas sem otimização, o que pode ajudar em problemas de carregamento
+  },
+  // Garantir que o publicPath esteja configurado corretamente
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
 };
 
 export default nextConfig;
